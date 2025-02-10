8825bc8c002670aaf66051abe93d463202af7822
RewDev <orbitamcnetwork@gmail.com>
2025-02-10 02:36:56 -0600

    Added: Path resolvers to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@Components": path.resolve(__dirname, "./src/shared/components"),
+      "@Buttons": path.resolve(__dirname, "./src/shared/components/buttons"),
+      "@Form": path.resolve(__dirname, "./src/shared/components/form"),
+      "@DataDisplay": path.resolve(__dirname, "./src/shared/components/data-display"),
+      "@Navigation": path.resolve(__dirname, "./src/shared/components/navigation"),
+      "@Structure": path.resolve(__dirname, "./src/shared/components/structure"),
+      "@Toast": path.resolve(__dirname, "./src/shared/components/toast/Toast"),
+
+    },
+  },
 })
